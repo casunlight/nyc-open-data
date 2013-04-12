fd193d6e11e84e22552ab4bb1470090659634b25
Thomas Levine <occurrence@thomaslevine.com>
2013-04-12 15:37:42 -0400

    header

--- table-features.r
+++ table-features.r
@@ -1,13 +1,16 @@
 #!/usr/bin/env Rscript
 
-prop.capital
+prop.cap
 
 table.features <- function(id){
   table <- read.csv(paste(id, '.csv', sep = ''))
+  header <- paste(colnames(table), collapse = '')
   c(
     id = id,
     nrow = nrow(table),
     ncol = ncol(table),
-
+    header.nchar = length(header),
+    header.nlowercase = length(gsub('[^a-z]', '', header)),
+    header.nlowercase = length(gsub('[^A-Z]', '', header)),
   )
 }
