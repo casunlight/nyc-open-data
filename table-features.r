5cd5d29b9ce03e92ae7929e6bc567e370fe3ef60
Thomas Levine <occurrence@thomaslevine.com>
2013-04-12 15:59:52 -0400

    more features

--- table-features.r
+++ table-features.r
@@ -1,16 +1,39 @@
 #!/usr/bin/env Rscript
+library(psych)
 
 table.features <- function(id){
-  table <- read.csv(paste(id, '.csv', sep = ''))
+  table <- read.csv(paste('rows/', id, '.csv', sep = ''), stringsAsFactors = F)
   header <- paste(colnames(table), collapse = '')
-  c(
+
+  # Year column
+  if ('year' %in% colnames(table)) {
+    year <- table$year
+  } else if ('Year' %in% colnames(table)) {
+    year <- table$Year
+  } else {
+    year <- NA
+  }
+
+  # Column types
+  col.types <- table(sapply(table, typeof))
+
+  features <- c(
     id = id,
     nrow = nrow(table),
     ncol = ncol(table),
-    header.nchar = length(header),
-    header.nlowercase = length(gsub('[^a-z]', '', header)),
-    header.nlowercase = length(gsub('[^A-Z]', '', header))
+    header.nchar = nchar(header),
+    header.nlowercase = nchar(gsub('[^a-z]', '', header)),
+    header.nlowercase = nchar(gsub('[^A-Z]', '', header)),
+    year.mean = mean(year),
+    year.median = median(year),
+    year.max = max(year),
+    year.min = min(year),
+    year.var = var(year),
+    year.skew = skew(year),
+    year.kurtosis = kurtosi(year),
+    ncol.character = col.types[['character']],
+    ncol.integer = col.types[['integer']]
   )
 }
 
-table.features('zpd4-gad8')
+t(t(table.features('zpd4-gad8')))
